ef39501eb790338127544f5ba4491cdc68db88ec
Castruu <vitorfycastro@hotmail.com>
2022-07-16 15:22:41 -0300

    Spring Rest and HAL Explorer

--- example49/src/main/java/com/example/spring/repository/CoursesRepository.java
+++ example49/src/main/java/com/example/spring/repository/CoursesRepository.java
@@ -2,11 +2,16 @@ package com.example.spring.repository;
 
 import com.example.spring.model.Courses;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.webmvc.RepositoryRestController;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
+@RepositoryRestResource(path = "courses"
+/*, exported = false*/
+)
 public interface CoursesRepository extends JpaRepository<Courses, Integer> {
 
     List<Courses> findByOrderByName();
